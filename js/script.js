7efc6f83a526fa445e53ac63c29f5640be0cd781
Maha Matin <matin643@gmail.com>
2016-10-27 12:50:52 -0700

    Fixed Typo

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
     //this refer to question in any list which I select in prev line
     //.next is selecting anything after every question in any list, which here is targetting answers (li.a)
     $(this).next()
+    //slideToggle for making answers slideUp and SlideDown with the speed defined in line 5
     .slideToggle(speed)
     .siblings('li.a')
     .slideUp()
