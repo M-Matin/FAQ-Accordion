036a47aed75649fc4220adcd7494b5eed1362bf6
Maha Matin <matin643@gmail.com>
2016-10-27 12:35:02 -0700

    define function for answers

--- js/script.js
+++ js/script.js
@@ -5,5 +5,12 @@ var action= "click";
 var speed= "500";
 
 $(document).ready(function(){
-  
+  //action is variable defined in line 4
+  $('li.q').on(action , function(){
+    //this refer to question in any list which I select in prev line
+    //.next is selecting anything after every question in any list, which here is targetting answers (li.a)
+    $(this).next()
+    .slideToggle(speed);
+
+  });
 });
