2454373dfe90ce70cef29758a20d1ac93cd892f1
Maha Matin <matin643@gmail.com>
2016-10-27 12:24:43 -0700

    Set action in JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//accordion
+//set some acction befor anything else
+//define how accordion will be open: by clicking, hovering, or ...
+var action= "click";
+var speed= "500";
+
+$(document).ready(function(){
+  
+});
